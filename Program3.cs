8fb36b4c700ade1b0c4562fce071bca16d54aae8
HASNA M S <61441880+mshasna@users.noreply.github.com>
2022-10-31 18:10:51 +0530

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace q3_MaximumEnrolement
+{
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Total Students Placed in CS: ");
+            int cs = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Total Students Placed in MECH: ");
+            int mech = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Total Students Placed in MET: ");
+            int met = Convert.ToInt32(Console.ReadLine());
+            int highest = cs;
+            if (cs > mech)
+            {
+                if (cs > met)
+                {
+                    Console.WriteLine("Highest placement CS");
+                }
+                else if (cs == met)
+                {
+                    Console.WriteLine("Highest placement CS");
+                    Console.WriteLine("Highest placement MET");
+                }
+                else
+                {
+                    Console.WriteLine("Highest placement MET");
+                }
+
+            }
+            else if (mech > cs)
+            {
+                if (mech > met)
+                {
+                    Console.WriteLine("Highest placement MECH");
+                }
+                else if (mech == met)
+                {
+                    Console.WriteLine("Highest placement MECH");
+                    Console.WriteLine("Highest placement MET");
+                }
+                else
+                {
+                    Console.WriteLine("Highest placement MET");
+                }
+            }
+            else
+            {
+                if (mech > met)
+                {
+                    Console.WriteLine("Highest placement CS");
+                    Console.WriteLine("Highest placement MECH");
+                }
+                else if (mech == met)
+                {
+                    Console.WriteLine("Highest placement CS");
+                    Console.WriteLine("Highest placement MECH");
+                    Console.WriteLine("Highest placement MET");
+                }
+                else
+                {
+                    Console.WriteLine("Highest placement MET");
+                }
+            }
+
+
+
+        }
+
+    }
+}
